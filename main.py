ed9462617c4d645ebcb6ca6024a060462d7665ea
Marc Brouard <Marc@ndmits-MacBook-Pro.local>
2022-10-25 15:34:24 +0100

    Changed to use python-decouple instead of dotenv

--- main.py
+++ main.py
@@ -10,25 +10,23 @@ Also includes a test_engine to be used when running the tests
 
 import sys
 from sqlalchemy import create_engine
-from dotenv import dotenv_values
 import oracledb
+from decouple import config
 
 oracledb.version = "19.0.0"
 sys.modules["cx_Oracle"] = oracledb
 
-config = dotenv_values(".env")
-
 
 engine = create_engine(
-    f'oracle://{config["DB_USER"]}:{config["DB_PASS"]}@',
+    f'oracle://{config("DB_USER")}:{config("DB_PASS")}@',
     connect_args={
-        "host": config["DB_HOST"],
-        "port": config["DB_PORT"],
-        "protocol": config["DB_PROTOCOL"],
+        "host": config("DB_HOST"),
+        "port": config("DB_PORT"),
+        "protocol": config("DB_PROTOCOL"),
         "retry_count": 20,
         "retry_delay": 3,
-        "service_name": config["SERVICE_NAME"],
-        "ssl_server_cert_dn": config["SSL_SERVER_CERT_DN"],
+        "service_name": config("SERVICE_NAME"),
+        "ssl_server_cert_dn": config("SSL_SERVER_CERT_DN"),
     },
 )
 
